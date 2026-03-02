50e6eea912b2719132bf92ba3b3f96ce6bea4ab5
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-03-02 10:07:25 +0700

    ggk

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Get('users')
+  @Get('test')
   test() {
     return this.userService.test();
   }
